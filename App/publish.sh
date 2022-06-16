a02295a7b3b5ff7e1090735e34d79c9f1419eaa0
ncn-ends <ncn949494@gmail.com>
2022-06-16 05:48:20 -0700

    improved publish script and related structure

--- App/publish.sh
+++ App/publish.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishReadyToRun=true -o ~/code/CliWrapper/out
+# gets the path of this bash file
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+# removes the /App directory from the path and then adds /releases
+PATH_TO_RELEASES=${SCRIPT_DIR::-4}"/releases"
+
+dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishReadyToRun=true -o $PATH_TO_RELEASES
