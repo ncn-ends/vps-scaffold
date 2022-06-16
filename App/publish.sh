2edfb0d5ff873a49f0ce06d0ffb7d7aefffb2ad0
ncn-ends <ncn949494@gmail.com>
2022-06-16 05:52:03 -0700

    releases temp

--- App/publish.sh
+++ App/publish.sh
@@ -4,6 +4,6 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
 # removes the /App directory from the path and then adds /releases
-PATH_TO_RELEASES=${SCRIPT_DIR::-4}"/releases"
+PATH_TO_RELEASES=${SCRIPT_DIR::-4}"/app-releases"
 
 dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishReadyToRun=true -o $PATH_TO_RELEASES
